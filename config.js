26b378ad3fd5ce32c4a3586729f3ead426eba2c5
Yaroslav Klymenko <yaroslaw.klymko@gmail.com>
2020-01-30 14:01:19 +0200

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    hashSalt: "45%sAlT_",
-    pageSize: 4,
-    // claudinary
-    cloud_name: "yarklym", 
-    api_key: "227437672474164",
-    api_secret: "1yBb8flnZo_XSkh_v7jFQssSvGA",
-    botConfig : "1062382302:AAEiEwWaRhWSrDAOrZR2PENMfhNynSVYs0U",
-};
